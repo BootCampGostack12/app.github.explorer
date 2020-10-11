5904a52d7600fbacabf601f88967a2bc27410f4c
mpgxc <mpgx5.c@gmail.com>
2020-10-11 06:05:49 -0300

    feat: done dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import ImageLogo from '../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
-  return <Title>Opa</Title>;
+  return (
+    <>
+      <img src={ImageLogo} alt="logo" />
+      <Title>Explore os repositorios no GithubExplorer</Title>
+      <Form>
+        <input placeholder="Repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13613590?s=400&u=569912bcd54d3ce94e4f8d697cc1b05baf763fc0&v=4"
+            alt="profile"
+          />
+          <div>
+            <strong>mpgxc/Caesar.Cipher.Codenation</strong>
+            <p>Desafio submetido para AceleraDev ReactJS - Codenation</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13613590?s=400&u=569912bcd54d3ce94e4f8d697cc1b05baf763fc0&v=4"
+            alt="profile"
+          />
+          <div>
+            <strong>mpgxc/Caesar.Cipher.Codenation</strong>
+            <p>Desafio submetido para AceleraDev ReactJS - Codenation</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/13613590?s=400&u=569912bcd54d3ce94e4f8d697cc1b05baf763fc0&v=4"
+            alt="profile"
+          />
+          <div>
+            <strong>mpgxc/Caesar.Cipher.Codenation</strong>
+            <p>Desafio submetido para AceleraDev ReactJS - Codenation</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
