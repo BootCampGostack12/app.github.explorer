751e8e509235404ae772da5f353f6507493d4cdc
mpgxc <mpgx5.c@gmail.com>
2020-10-11 23:43:27 -0300

    feat: done global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import ImageBackGround from '../assets/background.svg';
 
 export default createGlobalStyle`
   *{
@@ -8,6 +9,17 @@ export default createGlobalStyle`
     box-sizing: border-box
   }
   body{
-    background: #F0F0F5;
+    background: #F0F0F5 url(${ImageBackGround} ) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px  Roboto, sans-serif;
+  }
+
+  #root{
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
   }
 `;
